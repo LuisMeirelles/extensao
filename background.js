b9dbbb09cba0e0f8cde3241e43ec0639fb27d351
LuisMeirelles <luis.gustavo.souto.meirelles@gmail.com>
2026-04-17 15:48:06 -0300

    limit permissions to kiwify dashboard and enable action only for matching pages

--- background.js
+++ background.js
@@ -1,5 +1,20 @@
 chrome.runtime.onInstalled.addListener(() => {
   console.log("Extensão instalada.");
+
+  chrome.action.disable();
+
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
+    chrome.declarativeContent.onPageChanged.addRules([
+      {
+        conditions: [
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { hostEquals: "dashboard.kiwify.com", schemes: ["https"] },
+          }),
+        ],
+        actions: [new chrome.declarativeContent.ShowAction()],
+      },
+    ]);
+  });
 });
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
